80b9d23d667311a1931f6865a1720ca2f6e54770
Coffeeteer <tgrieshiemer@gmail.com>
2016-10-31 23:14:08 -0400

    got the server to work :)

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
+'use strict';
+
 var express      = require('express');
 var bodyParser   = require('body-parser');
 var http         = require('http');
 var path         = require('path');
-var cookieParser = require();
-
-var app = express();
+var cookieParser = require('cookie-parser');
 
 var port = process.env.PORT || 3010;
+var app = express();
 
-app.get('/', function() {
-	app.render('./index.html');
+app.use(express.static( process.cwd() ));
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.get('/', function(req, res) {
+	res.render('index');
 });
 
 //console.log('process.env.PORT', process.env.PORT);
