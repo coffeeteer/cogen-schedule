3215806fdbe49ba30fc55a5676ce31fac5239b1f
Coffeeteer <tgrieshiemer@gmail.com>
2016-11-10 22:14:47 -0500

    trying mysql instead

--- server.js
+++ server.js
@@ -6,13 +6,29 @@ var http           = require('http');
 var path           = require('path');
 var cookieParser   = require('cookie-parser');
 var momentTimezone = require('moment-timezone');
-var Pool           = require('pg').Pool;
+var mysql		   = require('mysql');
 
 
 var port = process.env.PORT || 3010;
 var app = express();
 
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'cogen_schedule'
+});
+
+connection.connect(function(error) {
+	if(!!error){
+		console.log("There's an error.");
+	} else {
+		console.log("Connected");
+	}
+});
+
 //global.db = require("./models");
+//var models = require("./models");
 
 app.use(express.static( process.cwd() ));
 
@@ -22,6 +38,13 @@ app.use(bodyParser.urlencoded({
 
 app.get('/', function(req, res) {
 	res.render('index');
+	connection.query("SELECT * FROM cogen_schedule", function(error, rows, fields) {
+		if(!!error){
+			console.log('error in the query');
+		} else {
+			console.log('Successful query');
+		}
+	});
 });
 
 //console.log('process.env.PORT', process.env.PORT);
